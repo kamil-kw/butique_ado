801a787cecf451b7052a4ef54b1399289ecd5dbd
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2022-03-01 14:51:01 +0000

    Adding photos and database

--- butique_ado/settings.py
+++ butique_ado/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'home',
+    'products',
+    
 ]
 
 MIDDLEWARE = [
@@ -147,3 +149,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
