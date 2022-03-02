8a7eb7d4baa8cbae52db7a3dd536a84a32a46062
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2022-03-02 15:22:36 +0000

    Adding Class to admin

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,27 @@ from django.contrib import admin
 from .models import Product, Category
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+
+class ProductAdmin(admin.ModelAdmin):
+    """[class will tell admin which fields to dispay for product]"""
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    """[class will tell admin which fields to dispay for category]"""
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+    
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
