dcc7afc578b29cdc1081c8f697d2050dc37ed800
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2022-03-04 14:18:41 +0000

    Adding clothing categories dropdown filter

--- home/views.py
+++ home/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
-
 def index(request):
-    """[A view to return the index page]"""
+    """ A view to return the index page """
 
     return render(request, 'home/index.html')
