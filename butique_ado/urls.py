fa460b0e2c963043efd444da00e5f8d488fc7f7e
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2022-03-07 15:23:46 +0000

    Addinfree delivery function

--- butique_ado/urls.py
+++ butique_ado/urls.py
@@ -24,5 +24,4 @@ urlpatterns = [
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
-    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
