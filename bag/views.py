9add88d29e4d640c689c0ba272a63fa55fca5838
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2022-03-10 12:46:33 +0000

    Adding delivery procentage fix

--- bag/views.py
+++ bag/views.py
@@ -3,18 +3,17 @@ from django.contrib import messages
 
 from products.models import Product
 
-
 # Create your views here.
 
 def view_bag(request):
-    """[A view that renders the bag contents page]"""
+    """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
 
 def add_to_bag(request, item_id):
-    """[Add a quantity of the specified product to the shopping bag]"""
+    """ Add a quantity of the specified product to the shopping bag """
 
-    product = Product.objects.get(pk=item)
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -35,12 +34,14 @@ def add_to_bag(request, item_id):
             bag[item_id] += quantity
         else:
             bag[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+    
 
 def adjust_bag(request, item_id):
-    """[Add a quantity of the specified product to the specified amount]"""
+    """Adjust the quantity of the specified product to the specified amount"""
 
     quantity = int(request.POST.get('quantity'))
     size = None
@@ -66,7 +67,8 @@ def adjust_bag(request, item_id):
 
 
 def remove_from_bag(request, item_id):
-    """[Remove the item from the shopping bag]"""
+    """Remove the item from the shopping bag"""
+
     try:
         size = None
         if 'product_size' in request.POST:
@@ -82,7 +84,6 @@ def remove_from_bag(request, item_id):
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
-    
-    except Exception as e:
-        return HttpResponse(status-500)
 
+    except Exception as e:
+        return HttpResponse(status=500)
