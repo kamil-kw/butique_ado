22132e33b6171ede6d4c8425ad4b5b6b91dde0c1
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2022-03-14 12:31:22 +0000

    Adding stripe payments

--- checkout/views.py
+++ checkout/views.py
@@ -5,6 +5,7 @@ from .forms import OrderForm
 
 
 def checkout(request):
+    '''[checkout]'''
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
@@ -14,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KdBhFJpLRpYuHWcXWHsuHLal4S7saGZWSOMhTD3LAMow3nnu2KI5jf6WtAATXnzck4sBk2wP0KttkHc2S0CVpTt00q1Df2QyH',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
