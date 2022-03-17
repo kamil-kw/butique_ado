e5ad59bddd3d600b0de14cf65392ee5829e270a6
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2022-03-17 14:31:34 +0000

    Adding cache

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
@@ -13,7 +13,6 @@ import json
 
 @require_POST
 def cache_checkout_data(request):
-    '''[]'''
     try:
         pid = request.POST.get('client_secret').split('_secret')[0]
         stripe.api_key = settings.STRIPE_SECRET_KEY
@@ -27,9 +26,9 @@ def cache_checkout_data(request):
         messages.error(request, 'Sorry, your payment cannot be \
             processed right now. Please try again later.')
         return HttpResponse(content=e, status=400)
-    
+
+
 def checkout(request):
-    '''[checkout function ]'''
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
