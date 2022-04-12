d27eecb8e4baeb3843c52bd27195e01fe7bdae9a
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2022-04-12 14:49:36 +0000

    Removing uncomented line in the settings

--- butique_ado/settings.py
+++ butique_ado/settings.py
@@ -180,15 +180,6 @@ if 'USE_AWS' in os.environ:
         'CacheControl': 'max-age=94608000',
     }
 
-
-    # # Bucket Config
-    # AWS_STORAGE_BUCKET_NAME = 'kam-kw-boutique-ados'
-    # AWS_S3_REGION_NAME = 'eu-central-1'
-    # AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    # AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    # AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-    
-    
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'kam-kw-boutique-ados'
     AWS_S3_REGION_NAME = 'eu-central-1'
@@ -196,13 +187,6 @@ if 'USE_AWS' in os.environ:
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-    # # fix test from stack
-    # AWS_S3_SIGNATURE_VERSION = "s3v4"
-    # AWS_S3_ENCRYPTION = True
-    # AWS_S3_FILE_OVERWRITE = False
-    # AWS_DEFAULT_ACL = None
-    # DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-    # AWS_S3_ADDRESSING_STYLE = "path"
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
