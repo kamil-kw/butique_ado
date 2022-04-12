1d375fa20281be546cbf5679f6a4b0168ae10d72
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2022-04-12 18:08:03 +0000

    Database export

--- butique_ado/settings.py
+++ butique_ado/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['kam-kw-boutique-ado.herokuapp.com', 'localhost']
 
